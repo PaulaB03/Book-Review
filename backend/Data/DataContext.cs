14d8f7ae215466a15d6d859d252252e8cc7a4ad6
PaulaB03 <balan.paula1@gmail.com>
2024-01-11 22:27:18 +0200

    First commit

---
+++ backend/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ backend/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Category
+    {
+        [Key] 
+        [Column("category_id")] 
+        public int Category_Id { get; set; }
+
+        [Required]
+        [Column("category_name")]
+        public string Category_Name { get; set; }
+    }
+}
